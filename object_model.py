9753cdc8cf65531cc3dc7bda36a00bf7c5306b71
BrendanBuhmann <brendan.buhmann@mediamonks.com>
2023-08-28 15:51:34 -0700

    feat: extend model

--- object_model.py
+++ object_model.py
@@ -17,27 +17,35 @@ class fflPlayer():
     name:str
     id:str
     message:str
-    player_position:str
-    lineup_position:str
-    last_week_score:int
-    current_week_score:int
+    player_position_id:str
+    lineup_position_id:str
+    salary:int
+    injured:bool
     acquisition_unix:int
+    acquisition_type:str
     acquistion_date:str = None
+    player_position:str = None
+    lineup_position:str = None
     
     def __post_init__(self):
-        self.acquistion_date = datetime.utcfromtimestamp(self.acquisition_unix).strftime('%m-%d-%y')
-    
-    def get_position_label(self):
         POSITION_MAP = {0: 'QB', 1: 'QB', 2: 'RB', 3: 'WR', 4: 'TE', 5: 'K', 6: 'TE', 7: 'OP', 8: 'DT', 9: 'DE', 10: 'LB', 11: 'DL',12: 'CB', 13: 'S', 14: 'DB', 15: 'DP', 16: 'D/ST',  17: 'K', 18: 'P',  19: 'HC',  20: 'BE',  21: 'IR',  22: '',  23: 'RB/WR/TE',  24: 'ER',  25: 'Rookie', 'QB': 0, 'RB': 2, 'WR': 4, 'TE': 6, 'D/ST': 16, 'K': 17, 'FLEX': 23}
-        return POSITION_MAP[self.player_position]
+        self.player_position = POSITION_MAP[self.player_position_id]
+        self.lineup_position = POSITION_MAP[self.lineup_position_id]
+        self.acquistion_date = datetime.utcfromtimestamp(self.acquisition_unix).strftime('%m-%d-%y')
+        
     
 
 @dataclass(slots=True)
 class fflTeam():
     team_name:str
     team_number:str
+    division_id:int
     owner:fflOwner
+    division_name:str = None
     players:list(fflPlayer) = list()
+    
+    def __post_init__(self):
+        self.division_name = "Greater Santa Clara Area" if self.division_id==1 else "Norther Lights"
     
     def update_owner(
         self,
